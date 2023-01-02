d177c02d1792158d25b241de0142f97310b1a132
diego1medeiros <111666788+diego1medeiros@users.noreply.github.com>
2023-01-02 11:15:21 -0300

    teste git

--- oficina/src/main/java/br/com/oficina/acao/Acao.java
+++ oficina/src/main/java/br/com/oficina/acao/Acao.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Acao {
-
+//teste
 String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException ;
 
 }
